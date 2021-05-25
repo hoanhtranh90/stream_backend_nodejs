d598dc49fb4bd37465e816e31a3d9955666bb3a7
sangnk <nguyenkhacsang100@gmail.com>
2021-05-25 15:02:09 +0700

    fx

--- server.js
+++ server.js
@@ -11,8 +11,23 @@ const config = {
   },
   http: {
     port: 8000,
+    mediaroot: './media/', // Suggest writing
     allow_origin: '*',
   },
+  trans: { // Here, the parameter is a trans parameter, not a relay parameter. The hls configuration in the relay parameter is invalid
+    ffmpeg: 'C:/ProgramData/chocolatey/lib/ffmpeg/tools/ffmpeg/bin/ffmpeg.exe',//Indicates the FFmpeg location
+    tasks: [
+      {
+        app: 'live',
+        ac: 'aac',
+        vc: 'libx264',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
+  }
 }
 
 
